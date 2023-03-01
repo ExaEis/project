1ac9d1d25bcdd002dc9dfa4fe65957466d1e5c3b
lukas <benjamin.lukas2003@gmail.com>
2023-03-01 14:01:57 +0100

    Test-Verbesserung

--- vec-tests.py
+++ vec-tests.py
@@ -12,9 +12,9 @@ def test_mul():
 def test_rmul():
     assert 3*vector([1, 2, 3, 4]) == vector([3, 6, 9, 12])
     assert 2*vector([1, 1, 1, 3]) != vector([2, 1, 2, 6])
-    assert vector([1, 2, 3, 4]) * 0 == vector([0])
-    assert vector (1/3)*([9, 9, 9]) == vector([3, 3, 3])
-    assert vector 11*([0, 0, 0, 0]) == vector([0])
+    assert 0*vector([1, 2, 3, 4]) == vector([0])
+    assert (1/3)*vector([9, 9, 9]) == vector([3, 3, 3])
+    assert 11*vector([0, 0, 0, 0]) == vector([0])
 
 
 "sub_test"
