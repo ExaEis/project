9cc8fd6e577af0307a38d2445f878421013ebd14
lukas <benjamin.lukas2003@gmail.com>
2023-03-01 12:05:04 +0100

    Test vektor-multiplikation

--- vec-tests.py
+++ vec-tests.py
@@ -1,12 +1,18 @@
 #Test Vektor-Klassen
 
 from vec_class import *
-def test___mul__():
+def test_mul():
     assert vector([1, 2, 3, 4])*3 == vector([3, 6, 9, 12])
-    assert vector([1, 1, 1, 3])*2 == vector([2, 2, 2, 6])
+    assert vector([1, 1, 1, 3])*2 != vector([2, 1, 2, 6])
     assert vector([1, 2, 3, 4])*0 == vector([0])
     assert vector([3, 3, 3])*(1/3) == vector([1, 1, 1])
     assert vector([0, 0, 0, 0])*4 == vector([0])
 
 
-test___mul__()
+def test_rmul():
+    assert 3*vector([1, 2, 3, 4]) == vector([3, 6, 9, 12])
+    assert 2*vector([1, 1, 1, 3]) != vector([2, 1, 2, 6])
+    assert vector([1, 2, 3, 4]) * 0 == vector([0])
+    assert vector (1/3)*([9, 9, 9]) == vector([3, 3, 3])
+    assert vector 11*([0, 0, 0, 0]) == vector([0])
+
