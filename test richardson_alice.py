7526334d2f5a12299c6ddc5c1a4403285c2ee64b
thiex <thiex67@gmail.com>
2023-03-01 15:55:40 +0100

    test richardson verfahren

--- test richardson_alice.py
+++ test richardson_alice.py
@@ -6,14 +6,26 @@ from vec_class import *
 """test richardson Bsp Tafel"""
 A = csr_matrix([2, 2, 1, 2], [0, 1, 0, 1], [0, 2, 4])
 b = vector([2, 2])
-theta = 0.00000000000001
+theta = 0.29
 start_x = vector([0, 0])
-maxiter = 100000
-tol = 10e15
+maxiter = 1000000
+tol = 0.00001
 
 #Erwartung x = [0, 1]
 x = richardson(A, b, start_x, theta, tol, maxiter)
 print(x)
+
+"""Test Triagonalmatrix
+A = csr_matrix([2, 1, 1, 2, 1, 1, 2], [0, 1, 0, 1, 2, 1, 2], [0, 2, 5, 7])
+b = vector([4, 4, 4])
+theta = 0.00001
+start_x = [0, 0, 0]
+maxiter = 10000
+tol = 0.00001
+
+x = richardson(A, b, start_x, theta, tol, maxiter)
+print(x)"""
+
 
 """Test Einheitsmatrix
 A1 = csr_matrix([1, 1, 1], [0, 1, 2], [0, 1, 2, 3])
@@ -24,5 +36,5 @@ maxiter1 = 100000
 tol1 = 10e15
 
 #Erwartet x = 0
-x = richardson(A1, b1, start_x1, theta1, tol1,maxiter1)
+x = richardson(A1, b1, start_x1, theta1, tol1, maxiter1)
 print (x)"""
