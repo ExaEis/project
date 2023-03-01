93b0653ea286767b7220041e54539d6d4ef18e3d
lukas <benjamin.lukas2003@gmail.com>
2023-03-01 15:29:01 +0100

    Verbesserung Vektor Test

--- vec-tests.py
+++ vec-tests.py
@@ -26,13 +26,13 @@ def test_add():
     assert vector([-5, 3]) + vector([3, -2]) == vector([-2, 1])
 test_add()
 
-def test__sub__():
+def test_sub():
     assert vector([1, 1, 1]) == vector([1, 2, 3]) - vector([0, 1, 2])
     assert vector([1, 2]) == vector([2, 4]) - vector([1, 2])
     assert vector([1, 3, 5, 3, 1]) == vector([3, 5, 7, 5, 3]) - vector([2, 2, 2, 2, 2])
     assert vector([2, 3, 5, 1]) == vector([5, 4, 8, 7]) - vector([3, 1, 3, 6])
     assert vector([6]) == vector([9]) - vector([3])
-test__sub__()
+test_sub()
 
 def test_norm():
     assert vector([3, 4]).__norm__() == 5
