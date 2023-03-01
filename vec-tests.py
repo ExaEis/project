6acc1ea8aa99050f6f3b12f397de314eddeb7433
lukas <benjamin.lukas2003@gmail.com>
2023-03-01 14:14:24 +0100

    Test-Verbesserung

--- vec-tests.py
+++ vec-tests.py
@@ -18,9 +18,13 @@ def test_rmul():
     assert 11*vector([0, 0, 0, 0]) == vector([0, 0, 0, 0])
 test_rmul()
 
-"sub_test"
-
-#from vec_class import *
+def test_add():
+    assert vector([1, 2, 3]) + vector ([0, 1, 2]) == vector([1, 3, 5])
+    assert vector([6]) + vector([3]) == vector([9])
+    assert vector([0, 0, 0]) + vector([1, 2, 3]) == vector([1, 2, 3])
+    assert vector([3, 3, 1]) + vector([1, 2, 1]) != vector([3, 3, 5])
+    assert vector([-5, 3]) + vector([3, -2]) == vector([-2, 1])
+test_add()
 
 def test__sub__():
     assert vector([1, 1, 1]) == vector([1, 2, 3]) - vector ([0, 1, 2])
@@ -29,14 +33,6 @@ def test__sub__():
     assert vector([2, 3, 5, 1]) == vector([5, 4, 8, 7]) - vector([3, 1, 3, 6])
     assert vector([6]) == vector([9]) - vector([3])
 test__sub__()
-
-def test_add():
-    assert vector([1, 2, 3]) + vector ([0, 1, 2]) == vector([1, 3, 5])
-    assert vector([6]) + vector([3]) == vector([9])
-    assert vector([0, 0, 0]) + vector([1, 2, 3]) == vector([1, 2, 3])
-    assert vector([3, 3, 1]) + vector([1, 2, 1]) != vector([3, 3, 5])
-    assert vector([-5, 3]) + vector([3, -2]) == vector([-2, 1])
-test_add()
 
 def test_norm():
     assert vector([3, 4]).__norm__() == 5
