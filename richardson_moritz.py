521362d1966b2ed78ad114a86a82378ef90a177e
senftleben <moritzsenftleben723@gmail.com>
2023-03-02 10:39:37 +0100

    endergebnis

--- richardson_moritz.py
+++ richardson_moritz.py
@@ -1,5 +1,5 @@
 from vec_class import *
-from lep_csr import *
+from csr_matrix import *
 def richardson(A, b, start_x, theta, tol, maxiter, p = 2):
     x = start_x
     for k in range(maxiter):
@@ -7,13 +7,14 @@ def richardson(A, b, start_x, theta, tol, maxiter, p = 2):
         if r.__norm__() < tol:
             break
         x = x - theta * r
-    return x, r.__norm__()
+    return x, r.__norm__(), k
 
-A = csr_matrix([1, 2, 2, 1, 1, 2, 2, 2], [0, 3, 1, 2, 3, 4, 0, 2], [0, 2, 4, 5, 6, 8])
-b = vector([1, 2, 0, 1, 1])
-start_x = vector([0, 0, 0, 0, 0])
-theta = 0.000000001
-tol = 0.000000000001
+
+A = csr_matrix(([4,3,1,4,3,1,4], [0,1,0,1,2,1,2], [0,2,5,7]))
+b = vector([2,2,2]) #wähle theta = 1/b+root(2ac)
+start_x = vector([0, 0, 0])
+theta = 0.15505
+tol = 0.001
 maxiter = 10000
 
 x = richardson(A, b, start_x, theta, tol, maxiter)
